d63ba7666867931efbb3d74474577c901b4591d6
Angelhmm <user@gmail.com>
2023-08-22 18:33:01 -0600

    M

--- extension/background.js
+++ extension/background.js
@@ -1,50 +1,81 @@
-let ctx = {
-  client_id: "client_id",
-};
+/**
+ *
+ *  💡 INTERCEPT RESPONSES - TO LOCAL POXY
+ */
 
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  async function (details) {
-    console.log(details);
-    if (!details.url.includes("cloudfront.net")) return;
+const filterUrl = "";
+const baseUrl = "http://localhost:4000/graphql";
 
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-      if (details.requestHeaders[i].name === "Authorization") {
-        try {
-          const response = await fetch(
-            `http://localhost:9090/your-endpoint?client=${ctx.client_id}`,
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ v8: true }),
-            }
-          );
-
-          const data = await response.json();
-          console.log("Success:", data);
-          details.requestHeaders[i].value = "Bearer " + data.token;
-        } catch (error) {
-          console.error("Error:", error);
-        }
-
-        break;
-      }
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    if (details.url.startsWith(filterUrl)) {
+      console.log("Original HTTP Request:", details);
+      const newUrl = `${baseUrl}/${details.url.replace(filterUrl, "")}`;
+      return { redirectUrl: newUrl };
     }
-
-    return { requestHeaders: details.requestHeaders };
   },
   { urls: ["<all_urls>"] },
-  ["blocking", "requestHeaders", "extraHeaders"]
+  ["blocking"]
 );
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("Message received!", request);
-  if (request.action === "doSomething") {
-    const result = "Some result";
+/**
+ *
+ *  💡 INTERCEPT RESPONSES - TO ADD HEADERS;
+ */
 
-    ctx.client_id = result;
+// let ctx = {
+//   client_id: "client_id",
+//   access_token: ""
+// };
 
-    sendResponse({ result: result });
-  }
-});
+// let token = null;
+// fetch(`http://localhost:9090/getToken?client=${ctx.client_id}`, {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     if (data?.access_token) {
+//       token = "Bearer " + data.access_token;
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+// chrome.webRequest.onBeforeSendHeaders.addListener(
+//   function (details) {
+//     if (!details.url.includes("graphql")) return {};
+
+//     const headerIndex = details.requestHeaders.findIndex(
+//       (h) => h.name.toLowerCase() === "authorization"
+//     );
+
+//     // if (headerIndex > -1 && token) {
+//     //   details.requestHeaders[headerIndex].name = "Authorization";
+//     //   details.requestHeaders[headerIndex].value = token;
+//     // }
+
+//     return { requestHeaders: details.requestHeaders };
+//   },
+//   { urls: ["<all_urls>"] },
+//   ["blocking", "requestHeaders", "extraHeaders"]
+// );
+
+/**
+ *
+ *  💡 Receive messages from content script;
+ */
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   console.log("Message received!", request);
+//   if (request.action === "doSomething") {
+//     const result = "Some result";
+
+//     ctx.client_id = result;
+
+//     sendResponse({ result: result });
+//   }
+// });
